dd739eda631183159b4892bda45982a51c5a6b0a
579957 <579957@ELF6IT215873.ACDM.DS.SAIT.CA>
2018-01-24 13:51:23 -0700

    Added LoginServlet Added LoginServlet

---
+++ src/java/servlets/LoginServlet.java
@@ -0,0 +1,34 @@
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author 579957
+ */
+public class LoginServlet extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        HttpSession session = request.getSession();
+        if (request.getAttribute("userId") != null) {
+                String userId = (String) session.getAttribute("userId");
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        
+        
+    }
+}
