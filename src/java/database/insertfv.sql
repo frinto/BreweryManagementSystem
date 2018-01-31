95b072a435c64c84b8bfddf6b486b8731f1f18a1
553817 <553817@ELF6IT215927.ACDM.DS.SAIT.CA>
2018-01-31 12:08:46 -0700

    Inserts Uploaded insert scripts for mock data

---
+++ src/java/database/insertfv.sql
@@ -0,0 +1,30 @@
+/*
+-- Query: SELECT * FROM brewery.fv
+LIMIT 0, 1000
+
+-- Date: 2018-01-31 11:48
+*/
+INSERT INTO `fv` (`fvId`,`volume`,`status`) VALUES (1,2000,'');
+INSERT INTO `fv` (`fvId`,`volume`,`status`) VALUES (2,2000,'');
+INSERT INTO `fv` (`fvId`,`volume`,`status`) VALUES (3,2000,'');
+INSERT INTO `fv` (`fvId`,`volume`,`status`) VALUES (4,2000,'');
+INSERT INTO `fv` (`fvId`,`volume`,`status`) VALUES (5,2000,'');
+INSERT INTO `fv` (`fvId`,`volume`,`status`) VALUES (6,2000,'');
+INSERT INTO `fv` (`fvId`,`volume`,`status`) VALUES (7,9000,'');
+INSERT INTO `fv` (`fvId`,`volume`,`status`) VALUES (8,9000,'');
+INSERT INTO `fv` (`fvId`,`volume`,`status`) VALUES (9,9000,'');
+INSERT INTO `fv` (`fvId`,`volume`,`status`) VALUES (10,9000,'');
+INSERT INTO `fv` (`fvId`,`volume`,`status`) VALUES (11,9000,'');
+INSERT INTO `fv` (`fvId`,`volume`,`status`) VALUES (12,4000,'');
+INSERT INTO `fv` (`fvId`,`volume`,`status`) VALUES (13,4000,'');
+INSERT INTO `fv` (`fvId`,`volume`,`status`) VALUES (14,4000,'');
+INSERT INTO `fv` (`fvId`,`volume`,`status`) VALUES (15,4000,'');
+INSERT INTO `fv` (`fvId`,`volume`,`status`) VALUES (16,4000,'');
+INSERT INTO `fv` (`fvId`,`volume`,`status`) VALUES (17,4000,'');
+INSERT INTO `fv` (`fvId`,`volume`,`status`) VALUES (18,4000,'');
+INSERT INTO `fv` (`fvId`,`volume`,`status`) VALUES (19,9000,'');
+INSERT INTO `fv` (`fvId`,`volume`,`status`) VALUES (20,9000,'');
+INSERT INTO `fv` (`fvId`,`volume`,`status`) VALUES (21,9000,'');
+INSERT INTO `fv` (`fvId`,`volume`,`status`) VALUES (22,9000,'');
+INSERT INTO `fv` (`fvId`,`volume`,`status`) VALUES (23,9000,'');
+INSERT INTO `fv` (`fvId`,`volume`,`status`) VALUES (24,9000,'');
