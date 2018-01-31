4ca8862bdfe41217d7be9002d3bd09f93d0815b0
553817 <553817@ELF6IT215927.ACDM.DS.SAIT.CA>
2018-01-31 12:35:07 -0700

    Inserts Uploaded insert scripts for mock data

---
+++ src/java/database/insertemployee.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: SELECT * FROM brewery.employee
+LIMIT 0, 1000
+
+-- Date: 2018-01-29 17:00
+*/
+INSERT INTO `employee` (`empId`,`firstName`,`lastName`,`password`,`email`,`roleId`,`isActive`,`resetPasswordUUID`) VALUES (1,'Matt','Davis','password','matt.davis@edu.sait.ca',1,1,NULL);
+INSERT INTO `employee` (`empId`,`firstName`,`lastName`,`password`,`email`,`roleId`,`isActive`,`resetPasswordUUID`) VALUES (2,'Martin','Czerwinski','password','martin.czerwinski@edu.sait.ca',1,1,NULL);
+INSERT INTO `employee` (`empId`,`firstName`,`lastName`,`password`,`email`,`roleId`,`isActive`,`resetPasswordUUID`) VALUES (3,'Jesse','Trieu','password','jessie.trieu@edu.sait.ca',2,1,NULL);
+INSERT INTO `employee` (`empId`,`firstName`,`lastName`,`password`,`email`,`roleId`,`isActive`,`resetPasswordUUID`) VALUES (4,'Huy','Le','Password','huy.le@edu.sait.ca',1,1,NULL);
+INSERT INTO `employee` (`empId`,`firstName`,`lastName`,`password`,`email`,`roleId`,`isActive`,`resetPasswordUUID`) VALUES (5,'Keegan','Evans','password','keegan.evans@edu.sait.ca',1,1,NULL);
