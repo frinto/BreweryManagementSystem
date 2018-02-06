9b7a9918357e84013ec86f25ad7be12fb1f5013a
553817 <553817@ELF6IT215927.ACDM.DS.SAIT.CA>
2018-02-05 19:27:36 -0700

    Scripts Added inserts for mock data

---
+++ src/java/database/insertbrewmaterials.sql
@@ -0,0 +1,66 @@
+/*
+-- Query: select * from brewmaterials
+LIMIT 0, 1000
+
+-- Date: 2018-02-05 18:50
+*/
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('2RowMalt',1000,'kg','malt');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('2RowMaltSilo',1000,'kg','malt');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('AcidulatedMalt',1000,'kg','malt');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('Amarillo',1000,'kg','hops');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('Amber',1000,'kg','malt');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('Azzaca',1000,'kg','hops');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('Belma',1000,'kg','hops');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('BlackMalt',1000,'kg','malt');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('CalciumChloride(CaCl2)',1000,'kg','chemical');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('CarastanMalt',1000,'kg','malt');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('Cascades',1000,'kg','hops');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('Centennial ',1000,'kg','hops');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('Chinook',1000,'kg','hops');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('ChocolateMalt',1000,'kg','malt');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('Citra',1000,'kg','hops');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('CO2 ',1000,'kg','gas');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('CorianderSeed',1000,'kg','additives');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('CrispBrownMalt',1000,'kg','malt');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('CrispPaleChocolate',1000,'kg','malt');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('Crystal120',1000,'kg','malt');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('Crystal40',1000,'kg','malt');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('Crystal75',1000,'kg','malt');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('DextraPils',1000,'kg','malt');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('EastKentGolding',1000,'kg','hops');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('Ella',1000,'kg','hops');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('FG Phosphoric(H3PO4)',1000,'kg','chemical');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('FoodGradeOxygen',1000,'kg','gas');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('FrancoBelgesSpecial Aroma',1000,'kg','malt');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('Fuggles',1000,'kg','hops');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('Galena',1000,'kg','hops');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('Gypsum(CaSO4)',1000,'kg','chemical');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('Hallertaur Blanc',1000,'kg','hops');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('LemonPeel',1000,'kg','additives');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('MarisOtter',1000,'kg','malt');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('MidnightWheat',1000,'kg','malt');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('Mosaic',1000,'kg','hops');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('Mt Hood',1000,'kg','hops');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('MunichMalt',1000,'kg','malt');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('PremiumPilsen',1000,'kg','malt');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('Raspberry(Squeeze)',1000,'kg','additives');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('RoastedBarley',1000,'kg','malt');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('RSBiscuit',1000,'kg','malt');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('RSNewdale',1000,'kg','malt');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('RSRoastedBarley',1000,'kg','malt');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('RyeMalt',1000,'kg','malt');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('Saaz',1000,'kg','hops');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('Simcoe',1000,'kg','hops');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('SimpsonsNaked Oats',1000,'kg','malt');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('SodiumChloride',1000,'kg','chemical');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('Sorachi Ace',1000,'kg','hops');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('SpecialB',1000,'kg','malt');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('SweetSpanishOrange Peel',1000,'kg','additives');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('Tahoma',1000,'kg','hops');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('Tettanang',1000,'kg','hops');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('VictoryMalt',1000,'kg','malt');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('ViennaMalt',1000,'kg','malt');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('Waimea',1000,'kg','hops');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('WeyermannCaramunich III',1000,'kg','malt');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('WheatMalt',1000,'kg','malt');
+INSERT INTO `brewmaterials` (`name`,`qty`,`units`,`type`) VALUES ('Willamette',1000,'kg','hops');
