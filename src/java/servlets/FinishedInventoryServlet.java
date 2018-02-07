f3f4d192e698efb53c57719f39fb107c6423807c
root <root@kali.sait.ca>
2018-02-07 11:20:56 -0700

    update by huy le working on retrieving data from database

--- src/java/servlets/FinishedInventoryServlet.java
+++ src/java/servlets/FinishedInventoryServlet.java
@@ -5,8 +5,14 @@
  */
 package servlets;
 
+import dataaccess.BrewDBException;
+import dataaccess.FinishedInventoryDB;
+import domainmodel.FinishedProduct;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -23,16 +29,27 @@ public class FinishedInventoryServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
-        request.setAttribute("message", "it works!");
-        
+
+        FinishedInventoryDB finishedDatabase = new FinishedInventoryDB();
+
+        try {
+            List<FinishedProduct> finishedProducts = finishedDatabase.getAllInventory();
+
+            request.setAttribute("finishedProducts", finishedProducts);
+            request.setAttribute("message", "it works!");
+
+        } catch (BrewDBException ex) {
+            Logger.getLogger(FinishedInventoryServlet.class.getName()).log(Level.SEVERE, null, ex);
+            request.setAttribute("message", "error retrieving finished product list from database");
+
+        }
+
         getServletContext().getRequestDispatcher("/WEB-INF/finishedInventory.jsp").forward(request, response);
-        
+
     }
-    
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
     }
 }
-
