691d0172a4da50960b87295b15c348f4b2822d73
root <root@kali.sait.ca>
2018-02-15 14:43:54 -0700

    finished inventory mock data push

---
+++ src/java/database/insertfinishedgoods.sql
@@ -0,0 +1,8 @@
+INSERT INTO `finishedproduct` (`productName`, `qty`) VALUES ('blonde 6 pack (flats)', 90);
+INSERT INTO `finishedproduct` (`productName`, `qty`) VALUES ('blonde 12 pack (flats)', 40);
+INSERT INTO `finishedproduct` (`productName`, `qty`) VALUES ('blacksmith', 19);
+INSERT INTO `finishedproduct` (`productName`, `qty`) VALUES ('wit', 91);
+INSERT INTO `finishedproduct` (`productName`, `qty`) VALUES ('bobby', 22);
+INSERT INTO `finishedproduct` (`productName`, `qty`) VALUES ('beachcomber', 10);
+INSERT INTO `finishedproduct` (`productName`, `qty`) VALUES ('neighbour', 13);
+INSERT INTO `finishedproduct` (`productName`, `qty`) VALUES ('gardener', 33);
