1fee5dcbc5d912712c4e07d68bd7d5bdaa16273b
root <root@kali.sait.ca>
2018-02-15 15:11:48 -0700

    mock data for account

--- src/java/database/insertaccount.sql
+++ src/java/database/insertaccount.sql
@@ -1,1 +1,5 @@
-INSERT INTO `account` VALUES (1,'roosevelt', '403-999-1111', 'address1');
+INSERT INTO `account` VALUES ('roosevelt', '403-999-1111', 'address1');
+INSERT INTO `account` VALUES ('ship n anchor', '403-999-1112', 'address2');
+INSERT INTO `account` VALUES ('liquor depot', '403-999-1113', 'address3');
+INSERT INTO `account` VALUES ('test', '403-999-1114', 'address4');
+INSERT INTO `account` VALUES ('test2', '403-999-1115', 'address5');
