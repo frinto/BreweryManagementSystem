30225c5a4989c1939014377b8ceb2067dcbfb313
727525 <727525@ELF6IT215913.ACDM.DS.SAIT.CA>
2018-02-16 12:46:47 -0700

    Tank Transfer - with working date functionality

---
+++ src/java/database/insertTransfers.sql
@@ -0,0 +1,4 @@
+INSERT INTO brewery.transfer (`transferId`, `date`, volume, `fromFv`, `toSv`) 
+	VALUES (1, '2018-02-15', 200, 1, 2);
+INSERT INTO brewery.transfer (`transferId`, `date`, volume, `fromFv`, `toSv`) 
+	VALUES (2, '2018-02-15', 340, 2, 1);
