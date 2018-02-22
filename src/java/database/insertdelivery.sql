76990bf4fd714aca0696125ae20c7a126ad0f13a
reare <reare@DESKTOP-SM7UFRQ>
2018-02-22 12:11:46 -0700

    updated moc data

--- src/java/database/insertdelivery.sql
+++ src/java/database/insertdelivery.sql
@@ -1,8 +1,8 @@
 INSERT INTO `delivery` VALUES (0, '2018-02-02', 'roosevelt', 1);
-INSERT INTO `delivery` VALUES (0, '2018-02-02', 'ship n anchor', 1);
-INSERT INTO `delivery` VALUES (0, '2018-02-02', 'liquor depot', 1);
-INSERT INTO `delivery` VALUES (0, '2018-02-02', 'test', 2);
-INSERT INTO `delivery` VALUES (0, '2018-02-03', 'test2', 3);
-INSERT INTO `delivery` VALUES (0, '2018-02-03', 'Huy Le Testing', 3);
-INSERT INTO `delivery` VALUES (0, '2018-02-03', 'test2', 3);
-INSERT INTO `delivery` VALUES (0, '2018-02-03', 'test2', 3);
+INSERT INTO `delivery` VALUES (1, '2018-02-02', 'ship n anchor', 1);
+INSERT INTO `delivery` VALUES (2, '2018-02-02', 'liquor depot', 1);
+INSERT INTO `delivery` VALUES (3, '2018-02-02', 'liqour place', 2);
+INSERT INTO `delivery` VALUES (4, '2018-02-03', 'national', 3);
+INSERT INTO `delivery` VALUES (5, '2018-02-03', 'malibu', 3);
+INSERT INTO `delivery` VALUES (6, '2018-02-03', 'windsor rose', 3);
+INSERT INTO `delivery` VALUES (7, '2018-02-03', 'cowboys', 3);
