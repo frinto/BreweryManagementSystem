b4fd33df04dd18148de3ec8fced86595ee840158
727525 <727525@ELF6IT215913.ACDM.DS.SAIT.CA>
2018-02-21 21:06:27 -0700

    tanktransfer update. database script update.

--- src/java/database/insertTransfers.sql
+++ src/java/database/insertTransfers.sql
@@ -1,4 +1,4 @@
-INSERT INTO brewery.transfer (`transferId`, `date`, volume, `fromFv`, `toSv`, `correction`) 
-	VALUES (1, '2018-02-15', 200, 1, 2, null);
-INSERT INTO brewery.transfer (`transferId`, `date`, volume, `fromFv`, `toSv`, `correction`) 
-	VALUES (2, '2018-02-15', 340, 2, 1, null);
+INSERT INTO brewery.transfer (`transferId`, `date`, volume, `fromFv`, `toSv`, `brand`, `correction`) 
+	VALUES (1, '2018-02-15', 200, 1, 2, 'Blonde', null);
+INSERT INTO brewery.transfer (`transferId`, `date`, volume, `fromFv`, `toSv`, `brand`, `correction`) 
+	VALUES (2, '2018-02-15', 340, 2, 1, 'Blacksmith', null);
