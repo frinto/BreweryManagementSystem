2c66ebc32991804b3d0b5a39af501c9502db965e
root <root@kali.sait.ca>
2018-02-21 19:47:19 -0700

    mock data for delivery update

--- src/java/database/insertdelivery.sql
+++ src/java/database/insertdelivery.sql
@@ -3,3 +3,6 @@ INSERT INTO `delivery` VALUES (0, '2018-02-02', 'ship n anchor', 1);
 INSERT INTO `delivery` VALUES (0, '2018-02-02', 'liquor depot', 1);
 INSERT INTO `delivery` VALUES (0, '2018-02-02', 'test', 2);
 INSERT INTO `delivery` VALUES (0, '2018-02-03', 'test2', 3);
+INSERT INTO `delivery` VALUES (0, '2018-02-03', 'Huy Le Testing', 3);
+INSERT INTO `delivery` VALUES (0, '2018-02-03', 'test2', 3);
+INSERT INTO `delivery` VALUES (0, '2018-02-03', 'test2', 3);
