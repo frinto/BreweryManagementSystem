6ea3007931091693351f54c5527a43cdc906bcbc
579957 <579957@ELF6IT215873.ACDM.DS.SAIT.CA>
2018-02-23 15:20:25 -0700

    Merge origin/master

---
+++ src/java/database/insertbrew.sql
@@ -0,0 +1,10 @@
+/*
+-- Query: select * from brew
+LIMIT 0, 1000
+
+-- Date: 2018-02-22 10:52
+*/
+INSERT INTO `brew` (`brewId`,`date`,`mashInTime`,`restTime`,`inTime`,`totalMashTime`,`underletTime`,`lauterRestTime`,`vorlaufTime`,`firstWortGravity`,`runOffTime`,`lastRunningsGravity`,`kettleFullVol`,`kettleFullGravity`,`kettleStrikeOutVol`,`kettleStrikeOutGravity`,`allInVolume`,`empId`,`fvId`,`recipeName`) VALUES (1,'2018-02-21',6,6,6,18,6,6,6,6,6,6,6,6,6,6,6,1,6,'Wit');
+INSERT INTO `brew` (`brewId`,`date`,`mashInTime`,`restTime`,`inTime`,`totalMashTime`,`underletTime`,`lauterRestTime`,`vorlaufTime`,`firstWortGravity`,`runOffTime`,`lastRunningsGravity`,`kettleFullVol`,`kettleFullGravity`,`kettleStrikeOutVol`,`kettleStrikeOutGravity`,`allInVolume`,`empId`,`fvId`,`recipeName`) VALUES (2,'2018-02-22',5,4,5,14,5,5,5,5,5,5,5,5,5,5,5,1,5,'Blonde');
+INSERT INTO `brew` (`brewId`,`date`,`mashInTime`,`restTime`,`inTime`,`totalMashTime`,`underletTime`,`lauterRestTime`,`vorlaufTime`,`firstWortGravity`,`runOffTime`,`lastRunningsGravity`,`kettleFullVol`,`kettleFullGravity`,`kettleStrikeOutVol`,`kettleStrikeOutGravity`,`allInVolume`,`empId`,`fvId`,`recipeName`) VALUES (3,'2018-02-22',7,7,7,21,7,7,7,7,7,7,7,7,7,7,7,1,7,'Blonde');
+INSERT INTO `brew` (`brewId`,`date`,`mashInTime`,`restTime`,`inTime`,`totalMashTime`,`underletTime`,`lauterRestTime`,`vorlaufTime`,`firstWortGravity`,`runOffTime`,`lastRunningsGravity`,`kettleFullVol`,`kettleFullGravity`,`kettleStrikeOutVol`,`kettleStrikeOutGravity`,`allInVolume`,`empId`,`fvId`,`recipeName`) VALUES (4,'2018-02-20',7,7,7,21,7,7,7,7,7,7,7,7,7,7,7,1,7,'Blacksmith');
