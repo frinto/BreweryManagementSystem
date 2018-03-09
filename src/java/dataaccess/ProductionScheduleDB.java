4ea4905b9e09319eb64178871388fb40b868d4fb
root <root@kali.sait.ca>
2018-03-08 23:14:16 -0700

    fixed bugs in raw inventory and update functionality fully functional in productionSchedule

--- src/java/dataaccess/ProductionScheduleDB.java
+++ src/java/dataaccess/ProductionScheduleDB.java
@@ -9,14 +9,15 @@ import domainmodel.Productionschedule;
 import java.util.List;
 import java.util.logging.*;
 import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
 
 /**
  *
  * @author root
  */
 public class ProductionScheduleDB {
-    
-        public List<Productionschedule> getAllInventory() throws BrewDBException {
+
+    public List<Productionschedule> getAll() throws BrewDBException {
         EntityManager em = DBUtil.getEmFactory().createEntityManager();
 
         try {
@@ -25,6 +26,25 @@ public class ProductionScheduleDB {
         } catch (Exception ex) {
             Logger.getLogger(FinishedInventoryDB.class.getName()).log(Level.SEVERE, "Cannot read production schedules", ex);
             throw new BrewDBException("Error getting finished product");
+        } finally {
+            em.close();
+        }
+    }
+    
+    public int update(Productionschedule productionschedule) throws BrewDBException
+    {
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        EntityTransaction trans = em.getTransaction();
+
+        try {
+            trans.begin();
+            em.merge(productionschedule);
+            trans.commit();
+            return 1;
+        } catch (Exception ex) {
+            trans.rollback();
+            Logger.getLogger(FinishedInventoryDB.class.getName()).log(Level.SEVERE, "Cannot update ", ex);
+            throw new BrewDBException("Error updating production schedule");
         } finally {
             em.close();
         }
