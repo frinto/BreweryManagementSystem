96977240b9b8110f2aeb2b45b348efc9d5d79465
root <root@kali.sait.ca>
2018-03-21 13:44:47 -0600

    manage Employee initial setup

---
+++ src/java/servlets/ManageEmployeeServlet.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author root
+ */
+@WebServlet(name = "ManageEmployeeServlet", urlPatterns = {"/ManageEmployeeServlet"})
+public class ManageEmployeeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+       
+
+        getServletContext().getRequestDispatcher("/WEB-INF/manageEmployee.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        
+    }
+
+}
---
+++ web/WEB-INF/manageEmployee.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : manageEmployee
+    Created on : Mar 21, 2018, 1:40:01 PM
+    Author     : root
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Manage Employee</title>
+    </head>
+    <body>
+        <h1>Manage Employee</h1>
+    </body>
+</html>
