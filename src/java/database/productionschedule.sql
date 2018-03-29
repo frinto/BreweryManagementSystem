a4a7d46ea006ba117bac9a4b58c2977ac9f62a7a
553817 <553817@ELF6IT215927.ACDM.DS.SAIT.CA>
2018-03-29 12:52:14 -0600

    Cleaning up UI UI

---
+++ src/java/database/productionschedule.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: select * from productionschedule
+LIMIT 0, 1000
+
+-- Date: 2018-03-29 12:50
+*/
+INSERT INTO `productionschedule` (`date`,`brew`,`fv`,`yst`,`transfer`,`fuge`,`clean`,`keg`,`bottle`,`can`) VALUES ('2018-03-05','','','','','','','','','');
+INSERT INTO `productionschedule` (`date`,`brew`,`fv`,`yst`,`transfer`,`fuge`,`clean`,`keg`,`bottle`,`can`) VALUES ('2018-03-06','','','','','','','','','');
+INSERT INTO `productionschedule` (`date`,`brew`,`fv`,`yst`,`transfer`,`fuge`,`clean`,`keg`,`bottle`,`can`) VALUES ('2018-03-07','','','','','','','','','');
+INSERT INTO `productionschedule` (`date`,`brew`,`fv`,`yst`,`transfer`,`fuge`,`clean`,`keg`,`bottle`,`can`) VALUES ('2018-03-08','','','','','','','','','');
+INSERT INTO `productionschedule` (`date`,`brew`,`fv`,`yst`,`transfer`,`fuge`,`clean`,`keg`,`bottle`,`can`) VALUES ('2018-03-09','','','','','','','','','');
