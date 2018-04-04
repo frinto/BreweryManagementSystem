c7b37cdeb46588e4abcf0d078fe5ceb48ff74056
579957 <579957@ELF6IT215873.ACDM.DS.SAIT.CA>
2018-04-04 13:36:16 -0600

    Completed javadoc for ProductionDB.java

--- src/java/dataaccess/ProductionDB.java
+++ src/java/dataaccess/ProductionDB.java
@@ -6,7 +6,6 @@
 package dataaccess;
 
 import domainmodel.Production;
-import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
@@ -19,7 +18,12 @@ import javax.persistence.EntityTransaction;
  * @author 579957
  */
 public class ProductionDB {
-    
+    /**
+     * Inserts a production into the database
+     * @param p production that gets inserted
+     * @return returns 1 if the insert was successful
+     * @throws BrewDBException sends a message that there was an error inserting a production
+     */
     public int insertProduction(Production p) throws BrewDBException {
         EntityManager em = DBUtil.getEmFactory().createEntityManager();
         EntityTransaction trans = em.getTransaction();
@@ -37,7 +41,12 @@ public class ProductionDB {
             em.close();
         }
     }
-    
+    /**
+     * Updates a production into the database
+     * @param p production that gets updated
+     * @return returns 1 if the update was successful
+     * @throws BrewDBException sends a message that there was an error updating the production
+     */
     public int updateProduction(Production p) throws BrewDBException {
         EntityManager em = DBUtil.getEmFactory().createEntityManager();
         EntityTransaction trans = em.getTransaction();
@@ -55,7 +64,11 @@ public class ProductionDB {
             em.close();
         }
     }
-    
+    /**
+     * Gets a list of all the productions in the database
+     * @return returns a list of all productions
+     * @throws BrewDBException sends a message that there was an error getting the list of productions
+     */
     public List<Production> getAllProduction() throws BrewDBException {
         EntityManager em = DBUtil.getEmFactory().createEntityManager();
         
@@ -69,7 +82,12 @@ public class ProductionDB {
             em.close();
         }
     }
-    
+    /**
+     * Gets a production with a specified id
+     * @param prodId the id of the production
+     * @return returns the production with the same id
+     * @throws BrewDBException sends a message that there was an error getting the production
+     */
     public Production getProduction(Integer prodId) throws BrewDBException {
         EntityManager em = DBUtil.getEmFactory().createEntityManager();
         try {
@@ -82,7 +100,12 @@ public class ProductionDB {
             em.close();
         }
     }
-    
+    /**
+     * Deletes a production from the database
+     * @param p the production that will be deleted
+     * @return returns 1 if the delete was successful
+     * @throws BrewDBException sends a message that there was an error deleting the production
+     */
     public int deleteProduction(Production p) throws BrewDBException {
         EntityManager em = DBUtil.getEmFactory().createEntityManager();
         EntityTransaction trans = em.getTransaction();
@@ -100,7 +123,13 @@ public class ProductionDB {
             em.close();
         }
     }
-    
+    /**
+     * Gets a list of productions within a certain range of dates
+     * @param minDate the start date range for the list of productions
+     * @param maxDate the end date range for the list of productions 
+     * @return returns the list of productions within the date range
+     * @throws BrewDBException sends a message that there was an error getting the list of productions
+     */
     public List<Production> getProdByDateRange (Date minDate, Date maxDate) throws BrewDBException {
         EntityManager em = DBUtil.getEmFactory().createEntityManager();
         try {
